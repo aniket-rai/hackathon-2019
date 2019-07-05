df64851f19ca1317726e5d21e0e3d6c8eab3364e
ChingisOtchigin <chingisotchigin@gmail.com>
2019-07-06 10:46:57 +1200

    yolo4

--- js/create-employee.js
+++ js/create-employee.js
@@ -2,6 +2,40 @@ document.addEventListener("DOMContentLoaded", function(){
     
 });
 
+function submitEmployee(){
+    load('employees').forEach(function(element){
+        if(element.email === document.getElementById('user_email').value){
+            return
+        }
+    })
+
+    fn = document.getElementById('first_name').value
+    ln = document.getElementById('last_name').value
+    e = document.getElementById('user_email').value
+    p = document.getElementById('temp_pass').value
+
+    if(fn && ln && e && p){
+        emp = load('employees')
+        emp.push({'firstname': fn, 'lastname': ln, 'password': p, 'email': e})
+        store('employees', emp)
+    }
+}
+
+function autoFill() {
+/* document.getElementById('temp_pass').value = generatePassword(); */
+document.getElementById('temp_pass').value = "temp123456789012"
+}
+
+function generatePassword() {
+var length = 16,
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+    passGen = "";
+for (var i = 0, n = chars.length; i < length; ++i) {
+    passGen += chars.charAt(Math.floor(Math.random() * n));
+}
+return passGen;
+}
+
 function store(key, val){
     localStorage.setItem(key, JSON.stringify(val))
 }
