22e84531b77f68a765557f966d49b507aadd3647
ChingisOtchigin <chingisotchigin@gmail.com>
2019-07-06 09:49:00 +1200

    yolo2

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
+    updateScore()
+    updateRating()
     //store('employees', [{'email': 'andreyborro@gmail.com', 'firstname': 'Andrey', 'lastname':'Borro', 'password': 'yeet'}])
 });
 
@@ -7,7 +9,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
+function updateScore(){
+    document.getElementById('perf').textContent = 'rate your performance today [ ' + document.getElementById('slider').value + ' / 5 ]'
+}
 
+function updateRating(){
+    scores = load('current_user').scores
+    sum = scores.reduce(function(a,b){
+        return a + b
+    })
+
+    document.getElementById('rating').textContent = (sum/scores.length).toFixed(2)
+}
 
 
 function store(key, val){
@@ -17,3 +30,4 @@ function store(key, val){
 function load(key){
     return JSON.parse(localStorage.getItem(key))
 }
+
