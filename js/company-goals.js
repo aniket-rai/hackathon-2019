1b2ed59cd19e7d372bea48bfac0fc762208bdd46
ChingisOtchigin <chingisotchigin@gmail.com>
2019-07-06 14:39:20 +1200

    yolox

---
+++ js/company-goals.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    g = load('goals')
+    document.getElementById('g1').textContent = g[0];
+    document.getElementById('g2').textContent = g[1];
+    document.getElementById('g3').textContent = g[2];
+
+});
