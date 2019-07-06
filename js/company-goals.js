5ed900bec6c2e86ff0af9431eb712373fd7c81ce
ChingisOtchigin <chingisotchigin@gmail.com>
2019-07-06 15:35:29 +1200

    yeet

--- js/company-goals.js
+++ js/company-goals.js
@@ -4,4 +4,14 @@ document.addEventListener("DOMContentLoaded", function(){
     document.getElementById('g2').textContent = g[1];
     document.getElementById('g3').textContent = g[2];
 
+    divs = load('divisions')
+    c = load('current_user')
+
+    divs.forEach(function(item){
+        if(item.name == c.div){
+            document.getElementById('d1').textContent = '[' + c.div + '] ' + item.goals[0];
+            document.getElementById('d2').textContent = '[' + c.div + '] ' + item.goals[1];
+            document.getElementById('d3').textContent = '[' + c.div + '] ' + item.goals[2];
+        }
+    })
 });
